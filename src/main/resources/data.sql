c266cf190b3c1197143fe60a51c506ade7b8d950
b18042 <e1b18042@oit.ac.jp>
2020-11-08 23:28:56 +0900

    データベースに新規テーブル・カラムを追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 INSERT INTO users (name) VALUES ('CPU');
 INSERT INTO users (name) VALUES ('しみず');
+INSERT INTO users (name) VALUES ('履歴書');
 INSERT INTO matches (user_1,user_2,user_1_hand,user_2_hand) VALUES (2,1,'Gu','Choki');
 INSERT INTO matches (user_1,user_2,user_1_hand,user_2_hand) VALUES (2,1,'Gu','Gu');
 INSERT INTO matches (user_1,user_2,user_1_hand,user_2_hand) VALUES (2,1,'Gu','Pa');
